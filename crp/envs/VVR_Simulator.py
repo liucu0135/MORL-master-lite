bcfe01e49ff38a5ea5a5057a1fc912ff1449ef6d
liucu0135 <darkcoke2005@163.com>
2020-09-22 13:23:04 +0800

    reward 0.2/0.8

--- crp/envs/VVR_Simulator.py
+++ crp/envs/VVR_Simulator.py
@@ -123,7 +123,7 @@ class VVR_Simulator():
         else:
             reward.append(1)
         # reward.append(-self.get_distortion())
-
+        step=0.2
         reward.append((1-self.get_distortion()/100)*(1-step)+step*(1-self.get_distortion(absolute=True, tollerance=15)/5))
         # reward.append(2-self.get_distortion(absolute=True, tollerance=15)/5)
         self.current_state=self.observe()
