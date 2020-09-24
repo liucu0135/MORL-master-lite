dfdc885c48e2e28588427c23630aaf62cd003c4c
liucu0135 <darkcoke2005@163.com>
2020-09-24 16:42:43 +0800

    dis_alert moved to dueling

--- crp/crl/envelope/models/conv.py
+++ crp/crl/envelope/models/conv.py
@@ -64,13 +64,13 @@ class EnvelopeConvCQN(torch.nn.Module):
             nn.ReLU(),
         ]
         fc_fuse = [
-            nn.Linear(256+self.m+self.m, 256),
+            nn.Linear(256, 256),
             nn.ReLU(),
             nn.Linear(256, 256-2),
             nn.ReLU(),
         ]
-        self.fca1 = nn.Linear(64, self.color_num)
-        self.fca2 = nn.Linear(64, self.color_num)
+        self.fca1 = nn.Linear(64+self.m, self.color_num)
+        self.fca2 = nn.Linear(64+self.m, self.color_num)
         # self.fca=nn.Linear(128+self.c, c)
         self.fcv1 = nn.Linear(64 + 1, 1)
         self.fcv2 = nn.Linear(64 + 1, 1)
@@ -134,22 +134,22 @@ class EnvelopeConvCQN(torch.nn.Module):
         hist2 = torch.min(tab, hist2)
         hist3 = torch.min(tab, hist3)
         x = torch.cat((tab, hist, hist2, hist3, last, dist_alert), dim=1)
-        x3= torch.cat((torch.sum(hist, dim=2).squeeze(1),dist_alert[:,0,0,:]), dim=1)
+        # x3= torch.cat((torch.sum(hist, dim=2).squeeze(1),dist_alert[:,0,0,:]), dim=1)
 
         x1 = self.conv1(x)
         x1 = self.fc1(x1.view(-1, 32 * self.c * self.m))
         x2 = self.conv0(state)
         x2 = self.fc0(x2.view(-1, 8 * self.fcnum))
-        x = self.fc_fuse(torch.cat((x1, x2, x3), dim=1))
+        x = self.fc_fuse(torch.cat((x1, x2), dim=1))
         x = torch.cat((x, preference), dim=1)
         v1 = self.fcv1(torch.cat((x[:, :64], step), dim=1))
-        a1 = self.fca1(x[:, 64:128])
+        a1 = self.fca1(torch.cat((x[:, 64:128],torch.sum(hist, dim=2).squeeze(1)),dim=1))
         # a=self.fca(torch.cat((x[:,128:], dist_alert[:,0,0,:]), dim=1))
         m1 = torch.mean(a1, dim=1).unsqueeze(1).repeat(1, a1.shape[1])
         a1 = a1 - m1
         q1 = a1 + v1
         v2 = self.fcv2(torch.cat((x[:, 128:64+128], step), dim=1))
-        a2 = self.fca2(x[:, 64+128:])
+        a2 = self.fca2(torch.cat((x[:, 64+128:],dist_alert[:,0,0,:]),dim=1))
         # a=self.fca(torch.cat((x[:,128:], dist_alert[:,0,0,:]), dim=1))
         m2 = torch.mean(a2, dim=1).unsqueeze(1).repeat(1, a2.shape[1])
         a2 = a2- m2
