9157b6f1f0857a8ae020f895197cbc7b37fa0526
liucu0135 <darkcoke2005@163.com>
2020-09-27 20:10:38 +0800

    dist alert fixed

--- crp/crl/envelope/meta.py
+++ crp/crl/envelope/meta.py
@@ -230,13 +230,13 @@ class MetaAgent(object):
             # # update mean
             # self.preference_mean=np.mean(np.abs(reward_data), axis=0)
 
-            w_batch = np.abs(np.random.normal(0,0.3,self.weight_num))
-            w_batch2 = np.abs(1-w_batch)
-            if np.random.rand()<0.5:
-                w_batch=np.column_stack((w_batch,w_batch2))
-            else:
-                w_batch=np.column_stack((w_batch2,w_batch))
-# print('size of wbatch',w_batch)
+            # w_batch = np.abs(np.random.normal(0,0.3,self.weight_num))
+            # w_batch2 = np.abs(1-w_batch)
+            # if np.random.rand()<0.5:
+            #     w_batch=np.column_stack((w_batch,w_batch2))
+            # else:
+            #     w_batch=np.column_stack((w_batch2,w_batch))
+            w_batch = np.abs(np.random.uniform(0,1,2))
 
             # w_batch=w_batch/np.repeat(np.expand_dims(np.sqrt(self.preference_mean)+0.0001,axis=0),self.weight_num,axis=0)
             # w_batch = (np.abs(np.random.multivariate_normal(self.preference_mean, self.preference_cov, size=self.weight_num)))
