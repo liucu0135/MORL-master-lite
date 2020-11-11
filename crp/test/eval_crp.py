d6c0bb5ac7686c60ae7e63ae5130861a2f0f8022
liucu0135 <darkcoke2005@163.com>
2020-11-11 17:38:23 +0800

    chapt 6

--- crp/test/eval_crp.py
+++ crp/test/eval_crp.py
@@ -46,7 +46,7 @@ parser.add_argument('--exact_orders', default='test/distribute_result_s6_1.csv',
                     help='address for saving trained models')
 
 
-parser.add_argument('--name', default='2dnorm_sample_shaped_nc', metavar='name',
+parser.add_argument('--name', default='sixchpt_nc', metavar='name',
                     help='specify a name for saving the model')
 # Useless but I am too laze to delete them
 parser.add_argument('--mem_size', type=int, default=1000, metavar='M',
