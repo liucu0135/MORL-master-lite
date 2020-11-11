0ea37660119e61bca4437e056d29546a355566d7
liucu0135 <darkcoke2005@163.com>
2020-11-11 23:17:45 +0800

    chapt 6

--- crp/test/eval_crp.py
+++ crp/test/eval_crp.py
@@ -42,9 +42,10 @@ parser.add_argument('--pltdemo', default=False, action='store_true',
 # LOG & SAVING
 parser.add_argument('--save', default='crl/envelope/saved2/', metavar='SAVE',
                     help='address for saving trained models')
-parser.add_argument('--exact_orders', default='test/distribute_result_s6_0.csv', metavar='SAVE',
+parser.add_argument('--exact_orders', default='test/distribute_result_s6_1.csv', metavar='SAVE',
                     help='address for saving trained models')
-
+parser.add_argument('--exact_orders2', default='test/distribute_result_s6_2.csv', metavar='SAVE',
+                    help='address for saving trained models')
 
 parser.add_argument('--name', default='sixchpt_nc', metavar='name',
                     help='specify a name for saving the model')
